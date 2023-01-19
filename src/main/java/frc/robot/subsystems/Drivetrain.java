2e5a4a54a8dd3f0ab94614f52b24faef7a151f3c
DaSausiBoi <61775079+DaSausiBoi@users.noreply.github.com>
2023-01-18 16:17:46 -0800

    SPARK MAX motor config example

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,5 +1,10 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import frc.robot.Constants.CanIdConstants;
+
 public class Drivetrain {
-    
+    private CANSparkMax rightLeader = new CANSparkMax(CanIdConstants.RIGHT_LEADER_ID, MotorType.kBrushless);
 }
