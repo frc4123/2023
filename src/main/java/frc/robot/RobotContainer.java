7dcb56d0a2f64b6cb0727b245a49019716ab6213
DaSausiBoi <61775079+DaSausiBoi@users.noreply.github.com>
2023-01-18 16:35:47 -0800

    first RobotContainer

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,50 @@
+package frc.robot;
+
+import static frc.robot.Constants.*;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.POVButton;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.Constants.UsbConstants;
+
+import frc.robot.subsystems.Drivetrain;
+
+public class RobotContainer {
+    private final Drivetrain drivetrain = new Drivetrain();
+
+    private final XboxController driverController = new XboxController(UsbConstants.DRIVER_CONTROLLER_PORT);
+    private final XboxController driverController2 = new XboxController(UsbConstants.AUXDRIVER_CONTROLLER_PORT);
+
+    public RobotContainer() {
+        // add negative (-) to getLeftY to invert drive (shooter will be the back, intake will be the front)
+        configureButtonBindings();
+    
+        drivetrain.arcadeDrive(driverController.getRightX(), driverController.getLeftY());
+      }
+
+      private void configureButtonBindings() {
+        Trigger lb = new JoystickButton(driverController, XboxConstants.LB_BUTTON);
+        Trigger rb = new JoystickButton(driverController, XboxConstants.RB_BUTTON);
+        Trigger a = new JoystickButton(driverController2, XboxConstants.A_BUTTON);
+        Trigger b = new JoystickButton(driverController2, XboxConstants.B_BUTTON);
+        Trigger x = new JoystickButton(driverController2, XboxConstants.X_BUTTON);
+        Trigger y = new JoystickButton(driverController2, XboxConstants.Y_BUTTON);
+        POVButton povUp = new POVButton(driverController2, 0);
+        POVButton povUpRight = new POVButton(driverController2, 45);
+        POVButton povRight = new POVButton(driverController2, 90);
+        POVButton povDownRight = new POVButton(driverController2, 135);
+        POVButton povDown = new POVButton(driverController2, 180);
+        POVButton povDownLeft = new POVButton(driverController2, 225);
+        POVButton povLeft = new POVButton(driverController2, 270);
+        POVButton povUpLeft = new POVButton(driverController2, 315);
+      }
+      
+      public Command getAutonomousCommand() {
+        return null;
+      }
+    
+}
