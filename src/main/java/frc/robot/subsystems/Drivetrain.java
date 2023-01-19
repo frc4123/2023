962ee7692fe8c1669d02caa473c31856a408b09d
DaSausiBoi <61775079+DaSausiBoi@users.noreply.github.com>
2023-01-18 16:35:18 -0800

    added motor following and acradeDrive

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -2,12 +2,26 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import frc.robot.Constants.CanIdConstants;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import static frc.robot.Constants.*;
 
 public class Drivetrain {
     private CANSparkMax rightLeader = new CANSparkMax(CanIdConstants.RIGHT_LEADER_ID, MotorType.kBrushless);
     private CANSparkMax rightFollower = new CANSparkMax(CanIdConstants.RIGHT_FOLLOWER_ID, MotorType.kBrushless);
     private CANSparkMax leftLeader = new CANSparkMax(CanIdConstants.LEFT_LEADER_ID, MotorType.kBrushless);
     private CANSparkMax leftFollower = new CANSparkMax(CanIdConstants.LEFT_FOLLOWER_ID, MotorType.kBrushless);
+
+    private final DifferentialDrive differentialDrive = new DifferentialDrive(leftLeader, rightLeader);
+
+    public Drivetrain() {
+        leftFollower.follow(leftLeader);
+        rightFollower.follow(rightLeader);
+    }
+        //left leader is speed, right leader is direction
+    public void arcadeDrive(double speed, double direction){
+        differentialDrive.arcadeDrive(speed, direction);
+      }
 }
