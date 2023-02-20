6bbff3701e5e3ece7d6164111c37e53e7ab1612e
DaSausiBoi <61775079+DaSausiBoi@users.noreply.github.com>
2023-02-20 11:36:04 -0800

    can id add and fix

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -62,7 +62,8 @@ public final class Constants {
         public static final int LEFT_LEADER_ID = 5;
         public static final int LEFT_FOLLOWER_ID = 6;
         public static final int VERT_ELEV_ID = 7;
-        public static final int INTAKE_LEADER_ID = 8;
-        public static final int HORIZ_ELEV_ID = 9;
+        public static final int HORIZ_ELEV_ID = 8;
+        public static final int INTAKE_LEADER_ID = 9;
+        public static final int WRIST_ID = 10;
     }
 }
