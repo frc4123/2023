aad02595b5e5e06eb4e0b6e23948721f79d0c498
DaSausiBoi <61775079+DaSausiBoi@users.noreply.github.com>
2023-02-20 09:52:55 -0800

    removed unused classes

--- src/main/java/frc/robot/subsystems/HorizElev.java
+++ src/main/java/frc/robot/subsystems/HorizElev.java
@@ -5,7 +5,6 @@ import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import frc.robot.Constants.CanIdConstants;
 
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static frc.robot.Constants.*;
