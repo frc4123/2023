6a4d784411846d6c06562044203c62c8d33631b1
DaSausiBoi <61775079+DaSausiBoi@users.noreply.github.com>
2023-02-21 16:41:55 -0800

    potential shuffleboard for wrist position

--- src/main/java/frc/robot/subsystems/Wrist.java
+++ src/main/java/frc/robot/subsystems/Wrist.java
@@ -3,11 +3,12 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import frc.robot.Constants.CanIdConstants;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 // import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 // import static frc.robot.Constants.*;
+import frc.robot.Constants.CanIdConstants;
 
 public class Wrist extends SubsystemBase{
     private CANSparkMax wrist = new CANSparkMax(CanIdConstants.WRIST_ID, MotorType.kBrushless);
@@ -23,4 +24,8 @@ public class Wrist extends SubsystemBase{
         //command below needs fixing
         wrist.set(velo);
     }
+
+    // public void execute() {
+    //     SmartDashboard.putBoolean("Is wrist down?", true);
+    // }
 }
